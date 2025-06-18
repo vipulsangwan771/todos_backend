1a867b9c4f784562915f41e804a6200e974375af
Vipul Sangwan <vipulsangwan771@gmail.com>
2025-06-18 22:00:19 +0530

    commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://react-todos-ldfi.onrender.com',
+        origin: ['https://react-todos-ldfi.onrender.com'],
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     },
 });
